dee655bc81d540ab91d49d32dcf106630d0a42f2
Madeline Stark <madeline.stark1@gmail.com>
2018-02-17 05:09:12 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,11 @@
-
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-board.each do |turn_count|
-  counter = 0
-if turn_count = "X" || "O"
-  counter += 1
-end
-end
-
 def current_player(board)
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  board.each do |turn_count|
+    counter = 0
+  if turn_count = "X" || "O"
+    counter += 1
+  end
+  end
   if turn_count.even? true
     return "X"
   else
